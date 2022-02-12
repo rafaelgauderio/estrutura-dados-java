667040bc94be7471073c8b7109f3bb32562c3ed3
Rafael De Luca <deluca1712@gmail.com>
2022-02-12 19:53:25 -0300

    Método enfileirar elemento na fila = enqueue

--- src/com/rafaeldeluca/fila/Fila.java
+++ src/com/rafaeldeluca/fila/Fila.java
@@ -13,7 +13,16 @@ public class Fila<T> extends EstruturaEstatica<T>{
 		super(capacity);
 	}
 	
-	
+	//enfileirar = enqueue = adiciona no fim da fila
+	public void enfileira(T element) {
+		
+		//this.elements[this.size] = element;
+		//this.size++;
+		
+		//método da adiciona verifica se precisa aumentar a capacidade da fila antes de adicionar
+		this.adiciona(element);
+		
+	}
 	
 	
 
--- src/com/rafaeldeluca/fila/application/Program01.java
+++ src/com/rafaeldeluca/fila/application/Program01.java
@@ -8,8 +8,21 @@ public class Program01 {
 		
 		Fila<String> fila = new Fila<String>(15);
 		
+		System.out.println("Fila = FIFO = First In, First Out");
+		System.out.println("Primeiro elemento a entrar é o primeiro a sair, como uma fila de banco.");
+		System.out.println("Elemento novo é adicionado ao final da fila.");
 		System.out.println(fila.estaVazia());
 		System.out.println(fila.tamanho());
+		
+		fila.enfileira("Primeiro da fila");
+		fila.enfileira("Segundo");
+		fila.enfileira("Último da fila");
+		
+		System.out.println(fila.estaVazia());
+		System.out.println(fila.tamanho());
+		System.out.println(fila);
+		
+		
 		
 		
 	}
